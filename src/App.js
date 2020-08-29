3c6e592abbdab77dd91ce13e8ca10f4c9206634a
nicoWeb31 <nico.riot@free.fr>
2020-08-29 16:00:58 +0200

    adding header componenent

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from 'react-router-dom';
-import ShopPage from "./pages/shop/ShopPage.component"
+import ShopPage from "./pages/shop/ShopPage.component";
+import Header from "./component/header/Header.component"
 
 
 function App() {
   return (
     <div>
+
+      <Header/>
+
       <Switch>
 
         <Route exact path="/" component={HomePage} />
---
+++ src/component/header/Header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./header.style.scss";
+import {Link} from "react-router-dom";
+import { ReactComponent as Logo} from "../../asset/logo/crown.svg"
+
+const Header = () => {
+    return (
+        <div className="_header">
+            <Link to="/" className="_logo-container">
+                <Logo className="_logo" />
+            </Link>
+            <div className="_options">
+                <Link className="_option" to="/shop">SHOP</Link>
+                <Link className="_option" to="/shop">CONTACT</Link>
+                
+
+
+            </div>
+        </div>
+    );
+}
+
+export default Header;
