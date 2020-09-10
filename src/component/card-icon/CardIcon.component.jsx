bdb6f9d608c268f12c5201f0eb5efe47fea6c210
nicoWeb31 <nico.riot@free.fr>
2020-09-10 20:06:16 +0200

    card icon connect redux

--- src/component/card-icon/CardIcon.component.jsx
+++ src/component/card-icon/CardIcon.component.jsx
@@ -4,19 +4,27 @@ import "./card-icon.style.scss";
 import { connect } from 'react-redux';
 import {toggleCardHidden} from "../../redux/cart/cart.actions"
 
-const CardIcon = ({toggle}) => {
+const CardIcon = ({toggle,itemCount}) => {
     return (
         <div className="_cart-icon" onClick={toggle}>
         <ShoppingIcon className="_shopping-icon"/>
-        <span className="_item-count">0</span>
+        <span className="_item-count">{itemCount}</span>
 
         </div>
     );
 }
 
 
+const mapStateToProps = ({cart}) =>({
+    itemCount : 
+    cart.cartItems.reduce((accumulatedQuantity,cart)=>
+        accumulatedQuantity + cart.quantity
+        
+    ,0)
+})
+
 const mapDispatchToProps = dispatch =>({
     toggle: ()=>dispatch(toggleCardHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CardIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CardIcon);
